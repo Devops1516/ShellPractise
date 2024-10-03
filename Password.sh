959baba19278703ff0c98efcf4b6b109561f251e
Kethan <Kethan@Kethan>
2024-10-03 05:58:06 -0400

    5th

--- Password.sh
+++ Password.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Please enter your username: "
-read USERNAME
+read -s USERNAME
 
 echo "Please enter your password: "
-read PASSWORD
+read -s PASSWORD
 
 echo "your username is :$USERNAME"
 echo "your password is :$PASSWORD"
