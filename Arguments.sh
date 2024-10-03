8a19f4b2f864252ced4339201a77f6da835b4ecf
Kethan <Kethan@Kethan>
2024-10-03 05:40:56 -0400

    arg

---
+++ Arguments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Conversation example
+# author KL
+
+Person1=$1
+Person2=$2
+
+echo "enter $1,$2"
+
+echo "$Person1: Hello $Person2"
+echo "$Person2: I'm fine $Person1, How are you"
+echo "$Person1: I'm fine thank you $Person2"
+echo "$Person2: $Person1, where have you been all these days"
+echo "$Person1: Yes $Person2 - I was out of station"
