fc2e670415bba97d7cbe50c870eb3ccaaeb2947e
Kethan <Kethan@Kethan>
2024-10-05 02:28:48 -0400

    variable

--- 6-Arraylist.sh
+++ 6-Arraylist.sh
@@ -3,6 +3,6 @@
 FRUIT=("apple" "Kiwi" "Orange")
 
 echo "1st fruit is ${FRUIT[0]}"
-echo "2nd fruit is $FRUIT[1]"
+echo "2nd fruit is ${FRUIT[1]}"
 echo "all fruit are ${FRUIT[@]}"
-echo "all fruit are $FRUIT[@]"
+echo "all fruit are ${FRUIT[@]}"
