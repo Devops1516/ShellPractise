642ce9f2f5953eb54ed1e2fa4a763410f7c67dd6
Kethan <Kethan@Kethan>
2024-10-05 02:26:58 -0400

    variable

--- 3-Variables1-Dry.sh
+++ 3-Variables1-Dry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Suresh=$Person1
-Ramesh=$Person2
+PERSON1=Suresh
+PERSON2=Ramesh
 
-echo "$Person1: Hello $Person2...Hru"
-echo "$Person2: Hi $Person1..I'm good Hru"
-echo "$Person1: I'm good $Person2..thnx wru been all these days"
-echo "$Person2: Yes, $Person1. I was out station for a while"
-echo "$Person1: fine $Person2...bye tc"
-echo "$Person2: you too $Person1"
+echo "$PERSON1: Hello $PERSON2...Hru"
+echo "$PERSON2: Hi $PERSON1..I'm good Hru"
+echo "$PERSON1: I'm good $PERSON2..thnx wru been all these days"
+echo "$PERSON2: Yes, $PESRON1. I was out station for a while"
+echo "$PERSON1: fine $PERSON2...bye tc"
+echo "$PERSON2: you too $PERSON1"
