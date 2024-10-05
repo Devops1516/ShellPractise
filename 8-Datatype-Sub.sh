13fe160907d461ef66625aee1b08b8b55776178b
Kethan <Kethan@Kethan>
2024-10-05 03:08:04 -0400

    variable

---
+++ 8-Datatype-Sub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUM1=$1
+NUM2=$2
+
+if(NUM1 -gt NUM2)
+
+    then SUB=$(($NUM1-$NUM2))
+    echo "Substraction of $NUM1 and $NUM2 is $SUB"
+else 
+    echo "$NUM2 is greater then $NUM1"
+
+fi
